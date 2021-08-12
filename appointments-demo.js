4917e01557174af4ddace01910552f838bcbfdfe
Alec M <alecmattu1234@gmail.com>
2021-08-12 11:32:50 -0400

    Add demo appointment dataset

---
+++ appointments-demo.js
@@ -0,0 +1,73 @@
+const data = {
+  "status": 200,
+  "data": {
+    "Employees": [
+      {
+        "ID": 1,
+        "Name": "Ben Mourside"
+      },
+      {
+        "ID": 2,
+        "Name": "Johnny Knoxville"
+      }
+    ],
+    "Appointments": [
+      {
+        "Date": "2021-08-12",
+        "Time": "08:00:00",
+        "Length": 60,
+        "EmployeeID": 1,
+        "Name": "Customer 01",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "06:00:00",
+        "Length": 60,
+        "EmployeeID": 1,
+        "Name": "Customer 02",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "08:30:00",
+        "Length": 60,
+        "EmployeeID": 1,
+        "Name": "Customer 03",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "13:15:00",
+        "Length": 60,
+        "EmployeeID": 2,
+        "Name": "Customer 04",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "10:00:00",
+        "Length": 60,
+        "EmployeeID": 1,
+        "Name": "Customer 05",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "11:00:00",
+        "Length": 60,
+        "EmployeeID": 2,
+        "Name": "Customer 06",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      },
+      {
+        "Date": "2021-08-12",
+        "Time": "12:00:00",
+        "Length": 60,
+        "EmployeeID": 1,
+        "Name": "Customer 07",
+        "Comment": "Note xyz 3e29wnd jsdnsa efkekaa 383383a ajdnak"
+      }
+    ]
+  }
+};
